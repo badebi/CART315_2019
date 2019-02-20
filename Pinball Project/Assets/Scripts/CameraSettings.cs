6402d90e437dd0e1e78600ab6a6b4e2ec80a1768
Ebby <ebi.badavi@yahoo.com>
2019-02-20 04:34:42 -0500

    Init Camera Script
    + Added Camera script to toggle between cameras

---
+++ Pinball Project/Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera thirdPersonCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.enabled = true;
+        thirdPersonCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            thirdPersonCamera.enabled = !thirdPersonCamera.enabled;
+        }
+    }
+}
